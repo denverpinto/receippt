c226d39add4618646bacdd37f4a270e98fc54de3
denverpinto <denpinto13@gmail.com>
2024-03-15 21:11:34 +0530

    test sync --exclude

--- database/scripts/__renamer.py
+++ database/scripts/__renamer.py
@@ -2,9 +2,9 @@ import os
 import json
 
 # index created for directory -
-path = "/Users/Denver/Desktop/RECEIPPT/receippt-flat-db/slides"
+slidesPath = "/Users/Denver/Desktop/RECEIPPT/receippt-flat-db/slides"
 
-files = os.listdir(path)
+files = os.listdir(slidesPath)
 
 for index, file in enumerate(files):
-    os.rename(os.path.join(path, file), os.path.join(path, "_".join(file.split(" "))))
+    os.rename(os.path.join(slidesPath, file), os.path.join(slidesPath, "_".join(file.split(" "))))
